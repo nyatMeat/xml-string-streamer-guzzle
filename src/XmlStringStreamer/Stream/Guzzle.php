0de26f8f2a76dc1ede6ca94da59af7c18f9eb732
akaluzhskiy <akaluzhskiy@iiko.ru>
2020-10-02 11:06:03 +0400

    adapt for guzzle 6

--- src/XmlStringStreamer/Stream/Guzzle.php
+++ src/XmlStringStreamer/Stream/Guzzle.php
@@ -3,65 +3,122 @@
 namespace Prewk\XmlStringStreamer\Stream;
 
 use Exception;
-use GuzzleHttp\Psr7;
+use GuzzleHttp\Psr7\CachingStream;
+use function GuzzleHttp\Psr7\stream_for;
 use Prewk\XmlStringStreamer\StreamInterface;
 
 class Guzzle implements StreamInterface
 {
-    /** @var StreamInterface */
-    private $stream;
+	/** @var \Psr\Http\Message\StreamInterface */
+	private $stream;
 
-    /** @var int */
-    private $readBytes = 0;
+	/** @var int */
+	private $readBytes = 0;
 
-    /** @var int */
-    private $chunkSize;
+	/** @var int */
+	private $chunkSize;
 
-    /** @var callable|null */
-    private $chunkCallback;
+	/** @var callable|null */
+	private $chunkCallback;
 
-    public function __construct($url, $chunkSize = 1024, $chunkCallback = null)
-    {
-        $this->chunkSize = $chunkSize;
-        $this->chunkCallback = $chunkCallback;
-        $this->stream = new Psr7\CachingStream(
-            Psr7\stream_for(fopen($url, 'r'))
-        );
-    }
 
-    public function setGuzzleStream($stream)
-    {
-        $this->stream = $stream;
-    }
+	/**
+	 * Create instance for psr7 stream
+	 * @param $fileUrl
+	 * @param int $chunkSize
+	 * @param null $chunkCallback
+	 * @return Guzzle
+	 */
+	public static function createForFile(string $fileUrl, $chunkSize = 1024, $chunkCallback = null)
+	{
+		return (new static())
+			->setStream(new CachingStream(
+				stream_for(fopen($fileUrl, 'r')
+				)))
+			->setChunkSize($chunkSize)
+			->setChunkCallback($chunkCallback);
+	}
 
-    public function getChunk()
-    {
-        if (!$this->stream->eof()) {
-            $buffer = $this->stream->read($this->chunkSize);
-            $this->readBytes += strlen($buffer);
+	/**
+	 * Create instance for psr7 stream
+	 * @param \Psr\Http\Message\StreamInterface $stream
+	 * @param int $chunkSize
+	 * @param null $chunkCallback
+	 * @return Guzzle
+	 */
+	public static function createForPsrStream(\Psr\Http\Message\StreamInterface $stream, $chunkSize = 1024, $chunkCallback = null)
+	{
+		return (new static())
+			->setStream($stream)
+			->setChunkSize($chunkSize)
+			->setChunkCallback($chunkCallback);
+	}
 
-            if (is_callable($this->chunkCallback)) {
-                call_user_func_array($this->chunkCallback, [$buffer, $this->readBytes]);
-            }
+	public function getChunk()
+	{
+		if (!$this->stream->eof())
+		{
+			$buffer = $this->stream->read($this->chunkSize);
+			$this->readBytes += strlen($buffer);
 
-            return $buffer;
-        } else {
-            return false;
-        }
-    }
+			if (is_callable($this->chunkCallback))
+			{
+				call_user_func_array($this->chunkCallback, [$buffer, $this->readBytes]);
+			}
 
-    public function isSeekable()
-    {
-        return $this->stream->isSeekable();
-    }
+			return $buffer;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
-    public function rewind()
-    {
-        if ($this->isSeekable() === false) {
-            throw new Exception('Attempted to rewind an unseekable stream.');
-        }
+	public function isSeekable()
+	{
+		return $this->stream->isSeekable();
+	}
 
-        $this->readBytes = 0;
-        $this->stream->rewind();
-    }
+	public function rewind()
+	{
+		if ($this->isSeekable() === false)
+		{
+			throw new Exception('Attempted to rewind an unseekable stream.');
+		}
+
+		$this->readBytes = 0;
+		$this->stream->rewind();
+	}
+
+	/**
+	 * @param \Psr\Http\Message\StreamInterface $stream
+	 * @return Guzzle
+	 */
+	protected function setStream(\Psr\Http\Message\StreamInterface $stream): Guzzle
+	{
+		$this->stream = $stream;
+		return $this;
+	}
+
+	/**
+	 * @param int $chunkSize
+	 * @return Guzzle
+	 */
+	protected function setChunkSize(int $chunkSize): Guzzle
+	{
+		$this->chunkSize = $chunkSize;
+		return $this;
+	}
+
+	/**
+	 * @param callable|null $chunkCallback
+	 * @return Guzzle
+	 */
+	protected function setChunkCallback(?callable $chunkCallback): Guzzle
+	{
+		$this->chunkCallback = $chunkCallback;
+		return $this;
+	}
+
+
 }
--- tests/Integration/GuzzleTest.php
+++ tests/Integration/GuzzleTest.php
@@ -10,68 +10,68 @@ use Prewk\XmlStringStreamer\Stream\Guzzle;
 
 class GuzzleTest extends TestCase
 {
-    /** @var string */
-    private $firstChunk = 'abcde';
+	/** @var string */
+	private $firstChunk = 'abcde';
 
-    /** @var string */
-    private $secondChunk = 'fghij';
+	/** @var string */
+	private $secondChunk = 'fghij';
 
-    /** @var string */
-    private $source;
+	/** @var string */
+	private $source;
 
-    protected function setUp(): void
-    {
-        parent::setUp();
+	protected function setUp(): void
+	{
+		parent::setUp();
 
-        $this->source = "data:text/plain,{$this->firstChunk}{$this->secondChunk}";
-    }
+		$this->source = "data:text/plain,{$this->firstChunk}{$this->secondChunk}";
+	}
 
-    /** @test */
-    public function it_rewinds_a_stream()
-    {
-        $stream = new Guzzle($this->source, 5);
+	/** @test */
+	public function it_rewinds_a_stream()
+	{
+		$stream = Guzzle::createForFile($this->source);
 
-        $this->assertEquals($this->firstChunk, $stream->getChunk());
-        $this->assertEquals($this->secondChunk, $stream->getChunk());
+		$this->assertEquals($this->firstChunk, $stream->getChunk());
+		$this->assertEquals($this->secondChunk, $stream->getChunk());
 
-        $stream->rewind();
+		$stream->rewind();
 
-        $this->assertEquals($this->firstChunk, $stream->getChunk());
-        $this->assertEquals($this->secondChunk, $stream->getChunk());
-    }
+		$this->assertEquals($this->firstChunk, $stream->getChunk());
+		$this->assertEquals($this->secondChunk, $stream->getChunk());
+	}
 
-    /** @test */
-    public function it_throws_an_exception_if_a_stream_is_non_seekable()
-    {
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('Attempted to rewind an unseekable stream.');
+	/** @test */
+	public function it_throws_an_exception_if_a_stream_is_non_seekable()
+	{
+		$this->expectException(Exception::class);
+		$this->expectExceptionMessage('Attempted to rewind an unseekable stream.');
 
-        $stream = new Guzzle($this->source);
+		$stream = Guzzle::createForFile($this->source);
+		$reflection = new \ReflectionClass(get_class($stream));
+		$reflectionProperty = $reflection->getProperty('stream');
+		$reflectionProperty->setAccessible(true);
+		$reflectionProperty->setValue($stream, new NoSeekStream(stream_for($this->source)));
+		$stream->rewind();
+	}
 
-        $stream->setGuzzleStream(
-            new NoSeekStream(stream_for($this->source))
-        );
+	/** @test */
+	public function it_returns_true_on_seekable_stream()
+	{
+		$stream = Guzzle::createForFile($this->source);
 
-        $stream->rewind();
-    }
+		$this->assertTrue($stream->isSeekable());
+	}
 
-    /** @test */
-    public function it_returns_true_on_seekable_stream()
-    {
-        $stream = new Guzzle($this->source);
+	/** @test */
+	public function it_returns_false_on_non_seekable_stream()
+	{
+		$stream = Guzzle::createForFile($this->source);
 
-        $this->assertTrue($stream->isSeekable());
-    }
+		$reflection = new \ReflectionClass(get_class($stream));
+		$reflectionProperty = $reflection->getProperty('stream');
+		$reflectionProperty->setAccessible(true);
+		$reflectionProperty->setValue($stream, new NoSeekStream(stream_for($this->source)));
 
-    /** @test */
-    public function it_returns_false_on_non_seekable_stream()
-    {
-        $stream = new Guzzle($this->source);
-
-        $stream->setGuzzleStream(
-            new NoSeekStream(stream_for($this->source))
-        );
-
-        $this->assertFalse($stream->isSeekable());
-    }
+		$this->assertFalse($stream->isSeekable());
+	}
 }
